5724c7e8791b2a85b978dffe80733ac272368868
Lautaro Urso <lautarourso@gmail.com>
2024-06-27 14:10:48 -0300

    Hice script de spawns y valor

--- Assets/Scripts/SpawnerScripts.cs
+++ Assets/Scripts/SpawnerScripts.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class SpawnerScripts : MonoBehaviour
 {
+
     public GameObject[] productos;
     public Transform position;
+    public Transform position2;
+    public Transform position3;
+    public Transform position4;
     // Start is called before the first frame update
     void Start()
     {
         GameObject prefab = productos[Random.Range(0, productos.Length)];
-        Instantiate(prefab, position);
+        Instantiate(prefab, position).tag = "Producto";
+        GameObject prefab2 = productos[Random.Range(0, productos.Length)];
+        Instantiate(prefab2, position2).tag = "Producto";
+        GameObject prefab3 = productos[Random.Range(0, productos.Length)];
+        Instantiate(prefab3, position3).tag = "Producto";
+        GameObject prefab4 = productos[Random.Range(0, productos.Length)];
+        Instantiate(prefab4, position4).tag = "Producto";
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Value.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Value : MonoBehaviour
+{
+    public int valor;
+
+    void Start()
+    {
+        // Aquí obtendremos los GameObjects instanciados por SpawnerScripts
+        GameObject[] instancias = GameObject.FindGameObjectsWithTag("Producto");
+
+        foreach (GameObject instancia in instancias)
+        {
+            int valorObjeto = instancias[0].GetComponent<Value>().valor;
+            int valorObjeto2 = instancias[2].GetComponent<Value>().valor;
+            int valorObjeto3 = instancias[3].GetComponent<Value>().valor;
+            int valorObjeto4 = instancias[4].GetComponent<Value>().valor;
+            Debug.Log(valorObjeto);
+            Debug.Log(valorObjeto2);
+            Debug.Log(valorObjeto3);
+            Debug.Log(valorObjeto4);
+
+        }
+
+    }
+}
