2df31c752298d27e29943ce626e2df60e1f803b3
aguskovaa <147621346+aguskovaa@users.noreply.github.com>
2024-06-27 09:01:32 -0300

    UI Y SPAWNER HECHOS + ASSETS A ESCALA

---
+++ Assets/Scripts/SpawnerScripts.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScripts : MonoBehaviour
+{
+    public GameObject[] productos;
+    public Transform position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject prefab = productos[Random.Range(0, productos.Length)];
+        Instantiate(prefab, position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
