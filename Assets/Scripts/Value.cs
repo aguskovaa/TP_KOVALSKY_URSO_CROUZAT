ac92b35cd8b0d3b1c8c122b3e939a2b9489d5647
Lautaro Urso <lautarourso@gmail.com>
2024-06-27 20:13:41 -0300

    Puse tags

--- Assets/Scripts/Value.cs
+++ Assets/Scripts/Value.cs
@@ -9,18 +9,17 @@ public class Value : MonoBehaviour
         // Aquí obtendremos los GameObjects instanciados por SpawnerScripts
         GameObject[] instancias = GameObject.FindGameObjectsWithTag("Producto");
 
-        foreach (GameObject instancia in instancias)
-        {
+        
             int valorObjeto = instancias[0].GetComponent<Value>().valor;
             int valorObjeto2 = instancias[2].GetComponent<Value>().valor;
             int valorObjeto3 = instancias[3].GetComponent<Value>().valor;
             int valorObjeto4 = instancias[4].GetComponent<Value>().valor;
-            Debug.Log(valorObjeto);
-            Debug.Log(valorObjeto2);
-            Debug.Log(valorObjeto3);
-            Debug.Log(valorObjeto4);
+            Debug.Log(valorObjeto + "v0");
+            Debug.Log(valorObjeto2 + "v2");
+            Debug.Log(valorObjeto3 + "v3");
+            Debug.Log(valorObjeto4 + "v4");
 
         }
 
     }
-}
+
