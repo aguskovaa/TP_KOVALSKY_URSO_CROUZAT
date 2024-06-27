c5fbaa43ce935b4ef0b8bd42a78ecd09b2a0ae2e
Lautaro Urso <lautarourso@gmail.com>
2024-06-27 20:20:38 -0300

    JAJAJJAJAJJAJA

--- Assets/Scripts/SpawnerScripts.cs
+++ Assets/Scripts/SpawnerScripts.cs
@@ -14,12 +14,19 @@ public class SpawnerScripts : MonoBehaviour
     void Start()
     {
         GameObject prefab = productos[Random.Range(0, productos.Length)];
+        prefab.SetActive(true);
         Instantiate(prefab, position).tag = "Producto";
+
         GameObject prefab2 = productos[Random.Range(0, productos.Length)];
+        prefab2.SetActive(true);
         Instantiate(prefab2, position2).tag = "Producto";
+
         GameObject prefab3 = productos[Random.Range(0, productos.Length)];
+        prefab3.SetActive(true);
         Instantiate(prefab3, position3).tag = "Producto";
+
         GameObject prefab4 = productos[Random.Range(0, productos.Length)];
+        prefab4.SetActive(true);
         Instantiate(prefab4, position4).tag = "Producto";
     }
 
--- Assets/Scripts/Value.cs
+++ Assets/Scripts/Value.cs
@@ -1,24 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Value : MonoBehaviour
 {
     public int valor;
-
+    public Text suma;
     void Start()
     {
         // Aquí obtendremos los GameObjects instanciados por SpawnerScripts
         GameObject[] instancias = GameObject.FindGameObjectsWithTag("Producto");
 
         
-            int valorObjeto = instancias[0].GetComponent<Value>().valor;
-            int valorObjeto2 = instancias[2].GetComponent<Value>().valor;
-            int valorObjeto3 = instancias[3].GetComponent<Value>().valor;
-            int valorObjeto4 = instancias[4].GetComponent<Value>().valor;
+            int valorObjeto = instancias[1].GetComponent<Value>().valor;
+            int valorObjeto2 = instancias[3].GetComponent<Value>().valor;
+            int valorObjeto3 = instancias[5].GetComponent<Value>().valor;
+            int valorObjeto4 = instancias[7].GetComponent<Value>().valor;
             Debug.Log(valorObjeto + "v0");
             Debug.Log(valorObjeto2 + "v2");
             Debug.Log(valorObjeto3 + "v3");
             Debug.Log(valorObjeto4 + "v4");
-
+        int suma2 = (Random.Range(valorObjeto, valorObjeto4)) ;
+        suma.text = "               +               ?               =              " + valorObjeto + suma2;
         }
 
     }
